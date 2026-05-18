de228dd9c5861682490d91d622d215a94952bd8d
Belen <belencelis239@gmail.com>
2026-05-18 13:18:49 -0300

    Cuarto commit, arreglo carrito

--- js/carrito.js
+++ js/carrito.js
@@ -16,58 +16,29 @@ function renderizarCarrito() {
 
     let total = 0;
 
-    if (carrito.length === 0) {
-        contenedorCarrito.innerHTML = `
-            <h2>
-                El carrito está vacío
-            </h2>
+    if (carrito.length === 0) { contenedorCarrito.innerHTML = `
+            <h2> El carrito está vacío</h2>
         `;
-
         totalCarrito.innerText = "Total: $0";
-
         return;
     }
 
-    carrito.forEach((producto, index) => {
-        total += producto.price;
-
+    carrito.forEach((producto, index) => { total += producto.price;
         contenedorCarrito.innerHTML += `
-            <div class="itemCarrito">
-                <img src="${producto.image}">
-
-                <div class="itemInfo">
-                    <h3>
-                        ${producto.title}
-                    </h3>
-
-                    <p>
-                        $${producto.price}
-                    </p>
+            <div class="itemCarrito"><img src="${producto.image}">
+                <div class="itemInfo"><h3>${producto.title}</h3>
+                    <p>$${producto.price}</p>
                 </div>
-
-                <button
-                    onclick="eliminarProducto(${index})"
-                    class="btnEliminar"
-                >
-                    Eliminar
-                </button>
+                <button onclick="eliminarProducto(${index})" class="btnEliminar"> Eliminar</button>
             </div>
         `;
     });
-
-    totalCarrito.innerText =
-        "Total: $" + total.toFixed(2);
+    totalCarrito.innerText = "Total: $" + total.toFixed(2);
 }
 
 /* ELIMINAR */
-function eliminarProducto(index) {
-    carrito.splice(index, 1);
-
-    localStorage.setItem(
-        "carrito",
-        JSON.stringify(carrito)
-    );
-
+function eliminarProducto(index) {carrito.splice(index, 1);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
     renderizarCarrito();
 }
 
@@ -75,16 +46,12 @@ function eliminarProducto(index) {
 btnComprar.addEventListener("click", () => {
     if (carrito.length === 0) {
         alert("El carrito está vacío");
-
         return;
     }
 
     alert("Compra realizada con éxito");
-
     carrito = [];
-
     localStorage.removeItem("carrito");
-
     renderizarCarrito();
 });
 
